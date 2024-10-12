34f4a761cbca595be0a562b3dd5243e56937d19b
admin <admin@DESKTOP-0RLMI8P>
2024-10-12 22:13:35 +0530

    Add project files.

---
+++ PracticeSelectClass/Practicecs.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace PracticeSelectClass
+{
+    public class Practicecs
+    {
+        public static void Main(string[] args)
+        {
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Url = "https://formy-project.herokuapp.com/";
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.XPath("//a[text()='Dropdown' and @class='btn btn-lg']")).Click();
+            driver.FindElement(By.XPath("//button[@id='dropdownMenuButton']")).Click();
+            //IWebElement hoverTo = driver.FindElement(By.XPath("//div[@class='dropdown-menu']/a[text()='File Upload']"));
+            WebDriverWait wait = new WebDriverWait(driver , TimeSpan.FromSeconds(10));
+            wait.Until(driver => driver.FindElement(By.XPath("//div[@class='dropdown-menu']/a[text()='File Upload']")).Enabled);
+            IWebElement hoverTo = driver.FindElement(By.XPath("//div[@class='dropdown-menu']/a[text()='File Upload']"));
+
+
+
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].click();", hoverTo);
+            //IWebElement chooseButton = driver.FindElement(By.XPath("//button[text()='Choose']"));
+            //chooseButton.SendKeys(@"C:\Users\admin\Desktop\Preparation\C# programs.txt");
+            //
+            IWebElement chooseButton = driver.FindElement(By.XPath("//input[@type='file']"));
+
+
+            // Specify the file path (ensure this file exists on your system)
+            string filePath = @"C:\Users\admin\Desktop\Preparation\C# programs.txt"; 
+
+            // Send the file path to the input element
+            chooseButton.SendKeys(filePath);
+
+
+
+
+
+        }
+        
+        
+    }
+}
---
+++ PracticeSelectClass/Program.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace practice;
+
+public class SelectClass
+{
+
+    public static void Pp1(string[] args)
+    {
+
+        IWebDriver driver = new ChromeDriver();
+        driver.Url = "https://demoqa.com/select-menu";
+        driver.Manage().Window.Maximize();
+        
+        IList<IWebElement> iframes = driver.FindElements(By.TagName("iframe"));
+        IWebElement header = driver.FindElement(By.TagName("h1"));
+        string headerId = header.GetAttribute("id");
+        string strg = driver.FindElement(By.TagName("h1")).GetAttribute("id");
+        for (int i =0;i<iframes.LongCount();i++)
+
+        {
+            driver.SwitchTo().Frame(i);
+            try
+            {
+                IWebElement drpDown = driver.FindElement(By.XPath("//select[@id='cars']"));
+                SelectElement sel = new SelectElement(drpDown);
+                sel.SelectByText("Volvo");
+                sel.SelectByText("Audi");
+                break;
+
+            }
+                catch(Exception e)
+
+            {
+                Console.WriteLine(e);
+
+            }
+            finally
+            {
+                driver.SwitchTo().DefaultContent();
+            }
+        }
+        
+        //IList<IWebElement> allInOne = sel.Options;
+        //foreach(IWebElement wl in allInOne )
+        //{
+        //    Console.WriteLine(wl.Text);
+        //}
+        Console.ReadLine();
+
+
+
+    }
+}
